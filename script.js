1b7c8da19ef703aa2b7f15ff2c025f67fb605f52
LauchaQ2 <89219479+LauchaQ2@users.noreply.github.com>
2021-10-07 16:41:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// <>
 
 var izq = document.getElementById("cubo").style.marginLeft;
 var tpp = document.getElementById("cubo").style.marginTop;
 
 var movimiento = 60;
 
-
 function right(){
     izq = parseInt(izq) + movimiento;
-    if(izq>760){
-    izq=760;
+    if(izq>740){
+    izq=740;
     }
-    if(izq<=760){
+    if(izq<=740){
     document.getElementById("cubo").style.marginLeft = parseInt(izq) + "px";
 } else {
     return;
@@ -35,7 +33,7 @@ function tp(){
     if(tpp<0){
     tpp=0;
     }
-    if(tpp<=360){
+    if(tpp<=340){
     document.getElementById("cubo").style.marginTop = parseInt(tpp) + "px";
 } else {
     return;
@@ -44,6 +42,15 @@ function tp(){
 
 function bottom(){
     tpp = parseInt(tpp) + movimiento;
+    if(tpp>340){
+    tpp=340;
+    }
+    if(tpp>=0){
+    document.getElementById("cubo").style.marginTop = parseInt(tpp) + "px";
+} else {
+    return;
+}
+}
     if(tpp>360){
     tpp=360;
     }
